ad977b0b9f315222cf9eb8a895ceb457b9806f53
Venu <muralavenu24@gmail.com>
2025-09-11 12:25:56 +0530

    code_is_ready_for2ndtime_docker_image_creation_on_AWS_REPO

--- model/models.py
+++ model/models.py
@@ -18,12 +18,8 @@ class ChangeFormat(BaseModel):
 
 class SummaryResponse(RootModel[list[ChangeFormat]]):
     pass
-
-'''Enum is called enumeration  used to avoid Typos (speelling mistakes)'''
 class PromptType(str, Enum):
     DOCUMENT_ANALYSIS = "document_analysis"
     DOCUMENT_COMPARISON = "document_comparison"
     CONTEXTUALIZE_QUESTION = "contextualize_question"
     CONTEXT_QA = "context_qa"
-
-
